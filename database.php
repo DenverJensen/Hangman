fe38f314810b060cf20635dd2f89c1a181decf56
DenverJensen <jensendenver@gmail.com>
2022-05-31 00:13:56 -0600

    db

--- database.php
+++ database.php
@@ -1,8 +1,4 @@
 <?php
-$server = "sql104.epizy.com";
-$username = "epiz_31705905";
-$password = "wHyNh7spZrIZD";
-$database = "epiz_31705905_denver";
 
 //establish PDO connection to infinity free
 try {
